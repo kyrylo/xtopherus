37ff0b0f2cd1386e817d565ec7a97198788f8583
Kyrylo Silin <kyrylosilin@gmail.com>
2013-07-31 22:40:32 +0300

    Help: add forgotten comma

--- lib/xtopherus/plugins/help.rb
+++ lib/xtopherus/plugins/help.rb
@@ -4,8 +4,8 @@ module Xtopherus
 
     set react_on: :channel
 
-    match /help\z/
-    method: :display_help
+    match /help\z/,
+          method: :display_help
 
     def display_help(m)
       m.reply 'https://github.com/kyrylo/xtopherus/wiki/Help'
