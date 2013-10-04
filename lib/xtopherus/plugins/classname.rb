410197f9ef446372d7803c6ecad814b3258f398c
Kyrylo Silin <kyrylosilin@gmail.com>
2013-10-05 01:49:15 +0300

    classname.rb: fix typos

--- lib/xtopherus/plugins/classname.rb
+++ lib/xtopherus/plugins/classname.rb
@@ -11,9 +11,9 @@ module Xtopherus
     def execute(m)
       agent = Mechanize.new
       agent.user_agent_alias = 'Linux Mozilla'
-      @agent.max_history = 0
+      agent.max_history = 0
 
-      page = @agent.get('http://www.classnamer.com/')
+      page = agent.get('http://www.classnamer.com/')
       m.reply(page.search("//p[@id='classname']").text)
     end
 
