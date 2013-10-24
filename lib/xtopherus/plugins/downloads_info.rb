b50f2e11ee346febba31edbbfe63993c2f006946
Kyrylo Silin <kyrylosilin@gmail.com>
2013-10-24 11:39:09 +0300

    DownloadsInfo: fix regular updates

--- lib/xtopherus/plugins/downloads_info.rb
+++ lib/xtopherus/plugins/downloads_info.rb
@@ -15,13 +15,15 @@ module Xtopherus
       @prev_downloads ||= (DownloadStamp.last || DownloadStamp.create(number: downloads))
       since_then = downloads - @prev_downloads.number
 
-      stamp = DownloadStamp.create(number: downloads)
-      days = ((stamp.created_at - @prev_downloads.created_at) / 86400).ceil
-      downloads = number_with_delimiter(downloads)
-      Channel(bot.channels.first).send(
-        "Pry was downloaded #{ numbers_with_delimiter(since_then) } times in " \
-        "#{ days } and #{ downloads } times in total.")
-      @prev_downloads = stamp
+      if since_then >= rand(100_000..110_000)
+        stamp = DownloadStamp.create(number: downloads)
+        days = ((stamp.created_at - @prev_downloads.created_at) / 86400).ceil
+        downloads = number_with_delimiter(downloads)
+        Channel(bot.channels.first).send(
+          "Pry was downloaded #{ number_with_delimiter(since_then) } times in " \
+          "#{ days } and #{ downloads } times in total. Good job, everyone!")
+        @prev_downloads = stamp
+      end
     end
 
     def report_downloads(m)
