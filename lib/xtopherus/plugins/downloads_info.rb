2fc425a462f04dcccf035c1172a3624aeb8f2190
Kyrylo Silin <kyrylosilin@gmail.com>
2013-10-24 03:01:31 +0300

    DownloadsInfo: don't use the cache

--- lib/xtopherus/plugins/downloads_info.rb
+++ lib/xtopherus/plugins/downloads_info.rb
@@ -5,31 +5,32 @@ module Xtopherus
 
     set react_on: :channel
 
-    match /downloads\z/,
-    method: :report_downloads
+    match /downloads\z/, method: :report_downloads
 
-    # 30 minutes.
-    timer 1_800, method: :send_downloads_notification
+    # 3 days.
+    timer 60 * 60 * 24 * 3, method: :send_downloads_notification
 
     def send_downloads_notification
-      downloads = Gems.total_downloads('pry', '0.9.12')[:total_downloads]
+      downloads = Gems.total_downloads('pry')[:total_downloads]
       @prev_downloads ||= (DownloadStamp.last || DownloadStamp.create(number: downloads))
       since_then = downloads - @prev_downloads.number
 
-      if since_then >= rand(45_000..55_000)
-        stamp = DownloadStamp.create(number: downloads)
-        days = ((stamp.created_at - @prev_downloads.created_at) / 86400).ceil
-        downloads = number_with_delimiter(downloads)
-        Channel(bot.channels.first).send(
-          "Pry was downloaded #{ numbers_with_delimiter(since_then) } times in " \
-          "#{ days } and #{ downloads } times in total.")
-        @prev_downloads = stamp
-      end
+      stamp = DownloadStamp.create(number: downloads)
+      days = ((stamp.created_at - @prev_downloads.created_at) / 86400).ceil
+      downloads = number_with_delimiter(downloads)
+      Channel(bot.channels.first).send(
+        "Pry was downloaded #{ numbers_with_delimiter(since_then) } times in " \
+        "#{ days } and #{ downloads } times in total.")
+      @prev_downloads = stamp
     end
 
     def report_downloads(m)
-      downloads = number_with_delimiter(DownloadStamp.last.number)
-      m.reply "Pry was downloaded about #{ downloads } times."
+      downloads = Gems.total_downloads('pry')
+      total_downloads = number_with_delimiter(downloads[:total_downloads])
+      version_downloads = number_with_delimiter(downloads[:version_downloads])
+      m.reply(
+        "Pry was downloaded about #{ total_downloads } times " \
+        "(#{ version_downloads } for the current version).")
     end
 
   end
